c00634ad6a641cb326e73a2b69b2deaa80913fb8
samuelrbdarco-cmd <samuelrbdarco@gmail.com>
2026-05-28 13:55:50 +0200

    fix: conflictos merge business

--- proyecto/bookings-backend-develop/backend/src/businesses/business.entity.ts
+++ proyecto/bookings-backend-develop/backend/src/businesses/business.entity.ts
@@ -1,4 +1,9 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
@@ -7,31 +12,46 @@ export class Business {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty({ example: 'Peluquería Nova', description: 'Nombre de la empresa' })
+  @ApiProperty({
+    example: 'Peluquería Nova',
+    description: 'Nombre de la empresa',
+  })
   @Column()
   name: string;
 
-  @ApiProperty({ example: 'Belleza & Estética', description: 'Categoría o actividad comercial' })
+  @ApiProperty({
+    example: 'Belleza & Estética',
+    description: 'Categoría o actividad comercial',
+  })
   @Column()
   category: string;
 
-  @ApiProperty({ example: 'contacto@peluquerianova.com', description: 'Correo electrónico de contacto' })
+  @ApiProperty({
+    example: 'contacto@peluquerianova.com',
+    description: 'Correo electrónico de contacto',
+  })
   @Column({ unique: true })
   email: string;
 
-  @ApiProperty({ example: '+34 600 123 456', description: 'Teléfono de contacto' })
+  @ApiProperty({
+    example: '+34 600 123 456',
+    description: 'Teléfono de contacto',
+  })
   @Column()
   phone: string;
 
-  @ApiProperty({ example: 'Cortes, peinados y tratamientos capilares de vanguardia.', description: 'Descripción de la empresa', nullable: true })
+  @ApiProperty({
+    example: 'Cortes, peinados y tratamientos capilares de vanguardia.',
+    description: 'Descripción de la empresa',
+    nullable: true,
+  })
   @Column({ nullable: true })
   description: string;
 
-  @ApiProperty({ example: '123456', description: 'Contraseña de la empresa' })
-  @Column({ default: '123456' })
-  password: string;
-
-  @ApiProperty({ example: '2026-05-14T08:00:00Z', description: 'Fecha de creación' })
+  @ApiProperty({
+    example: '2026-05-14T08:00:00Z',
+    description: 'Fecha de creación',
+  })
   @CreateDateColumn()
   createdAt: Date;
 }
